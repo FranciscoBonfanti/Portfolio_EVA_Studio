f197dc82b92d049fe4d9cd715d637e4ac1bf0382
User <user@Users-MacBook-Air.local>
2026-03-04 03:51:36 +0000

    img simuladas de proyectos agregados

--- src/components/ProjectsCarousel/ProjectsCarousel.jsx
+++ src/components/ProjectsCarousel/ProjectsCarousel.jsx
@@ -1,16 +1,39 @@
 import CircularGallery from './CircularGallery'
 
 export default function ProjectsCarousel() {
+
+     const proyectos = [
+    { image: '../../../public/img/img-carrusel-projects/metricas.jpeg', text: '' },
+    { image: '../../../public/img/img-carrusel-projects/landing.jpeg', text: '' },
+    { image: '../../../public/img/img-carrusel-projects/automatizacion.png', text: '' },
+    { image: '../../../public/img/img-carrusel-projects/leads.jpeg', text: ''  }
+  ]
+
   return (
     <section className="pcr" id="proyectos-carousel">
+
+    
+
+      <div className="container">
+        <div className="section-head">
+          <p className="kicker reveal" >Portfolio</p>
+          <h2 className="reveal" >Nuestro Proyectos</h2>
+          <p className="muted reveal"  >
+            Mirá algunos de nuestros proyectos más recientes
+          </p>
+        </div>
+      </div>
+
+
       <div style={{ height: '600px', position: 'relative' }}>
-        <CircularGallery 
-          bend={1}
+        <CircularGallery
+        items={proyectos}
+          bend={0}
           borderRadius={0.05}
-          scrollSpeed={2}
+          scrollSpeed={1}
           scrollEase={0.05}
         />
       </div>
     </section>
   )
-}
+} 
